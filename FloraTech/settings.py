d95154edd5c080ffdb503fd3092b3369fd9b6d02
frascalise <scalise92111@gmail.com>
2024-10-21 17:29:07 +0200

    Added Dashboard

--- FloraTech/settings.py
+++ FloraTech/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'FloraTech.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, "templates")
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,4 +125,3 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-os.path.join(BASE_DIR, "templates")
