d947b2649d2b2f3968b663ce963ee8996db0a1ef
frascalise <scalise92111@gmail.com>
2024-11-26 10:41:42 +0100

    Linked garden app in settings

--- FloraTech/settings.py
+++ FloraTech/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "appuser.apps.AppuserConfig",
-    "devices.apps.DevicesConfig"
+    "devices.apps.DevicesConfig",
+    "garden.apps.GardenConfig"
 ]
 
 MIDDLEWARE = [
