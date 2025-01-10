06a1468d866486064ee71fd48032d6389bccc654
asia <leuzzi.asia@gmail.com>
2025-01-11 00:17:35 +0100

    asia_dev: fixed chart

--- devices/views.py
+++ devices/views.py
@@ -36,26 +36,35 @@ def sensorDetails(request, parent_hub, id):
     sensor = SensorDevice.objects.get(id=id, parent_hub=parent_hub)
 
     s_telem = TelematryData.objects.filter(parent_sensor=id, parent_hub=parent_hub)
-    fig = px.line(
-        x=[t.received_date for t in s_telem],
-        y=[t.humidity for t in s_telem],
-        title="Andamento umidità",
-        labels={'x':'data ricezione', 'y':'valore umidità'}
-    )
+    if s_telem:
+        fig = px.line(
+            x=[t.received_date for t in s_telem],
+            y=[t.humidity for t in s_telem],
+            title="Andamento umidità",
+            labels={'x':'data ricezione', 'y':'valore umidità'}
+        )
 
-    fig.update_layout(title={
-        'font_size':22,
-        'xanchor':'center',
-        'x': 0.5}
-    )
+        fig.update_layout(title={
+            'font_size':22,
+            'xanchor':'center',
+            'x': 0.5}
+        )
 
-    chart = fig.to_html()
+        chart = fig.to_html()
 
-    context = {
-        "details": True,
-        "sensor": sensor,
-        "graph": chart
-    }
+        context = {
+            "details": True,
+            "sensor": sensor,
+            "graph": chart
+        }
+
+    else:
+        context = {
+            "details": True,
+            "sensor": sensor,
+            "graph": '<p>No data for this sensor</p>'
+        }
+
     return render(request, 'devices/sensor/sensorPage.html', context)
 
 @csrf_exempt
