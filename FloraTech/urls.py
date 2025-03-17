a55cfd0b0ca0cad082bb4a8d3ca8cbe8a3145916
frascalise <scalise92111@gmail.com>
2025-03-17 15:30:39 +0100

    Added garden page, small fixes and some adjustments

--- FloraTech/urls.py
+++ FloraTech/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import welcome_view, home_view
+from .views import welcome_view, home_view, garden_view
 
 urlpatterns = [
     path('', welcome_view, name='welcome'),
     path('home/', home_view, name='home'),
     path('accounts/', include('accounts.urls')),
+    path('garden/', garden_view, name='garden'),
     path('admin/', admin.site.urls),
 ]
--- FloraTech/views.py
+++ FloraTech/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from accounts.views import get_weather_forecast
 
 def welcome_view(request):
     if request.user.is_authenticated:
@@ -8,4 +9,9 @@ def welcome_view(request):
 
 @login_required
 def home_view(request):
-    return render(request, "home/home.html")
+    weather_data = get_weather_forecast(request)
+    return render(request, "home/home.html", {"weather": weather_data})
+
+@login_required
+def garden_view(request):
+    return render(request, "garden/garden.html")
