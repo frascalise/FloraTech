70507811306008d2e70fb01017e0b253210adfd7
BillyP42 <paolo.bigliardi91@gmail.com>
2025-04-14 15:51:14 +0200

    1

--- weather/meteo.py
+++ weather/meteo.py
@@ -7,7 +7,7 @@ def update():
     response = requests.get(url)
     json_result = response.json()
     print(json_result['daily']['weather_code'])
-    if json_result['daily']['weather_code'][4]=='63':WarningMessage()
+    if json_result['daily']['weather_code'][4]==63:WarningMessage()
     return json_result["daily"]["weather_code"],json_result["daily"]["time"]
 
 def richiesta_meteo():
