9ebc8379245003ce438cff015f7d8ea90a857363
BillyP42 <paolo.bigliardi91@gmail.com>
2025-04-14 16:13:38 +0200

    1

--- weather/BOT.py
+++ weather/BOT.py
@@ -7,7 +7,7 @@ import os
 import json
 TOKEN = '7789512707:AAFdHTHgdALOO745NlUPHftmClXrRBUMzjo'
 WEBHOOK_URL = 'https://floratech.leonardonels.com/weather' 
-USER_ID=1
+USER_ID=""
 
 # Inizializza il bot Telegram
 bot = telebot.TeleBot(TOKEN)
@@ -16,7 +16,7 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, "Ciao! Sono un bot webhook Django di prova.")
-    USER_ID=message.chat.id
+    USER_ID=str(message.chat.id)
 
 # Handler per il comando /help
 @bot.message_handler(commands=['help'])
