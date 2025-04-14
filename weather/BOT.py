47bfe11cb81b8a4ad7fbbcf9cd62b265640758c2
BillyP42 <paolo.bigliardi91@gmail.com>
2025-04-14 15:17:40 +0200

    1

--- weather/BOT.py
+++ weather/BOT.py
@@ -23,6 +23,7 @@ def send_help(message):
 
 @bot.message_handler(commands=['meteo'])
 def send_meteo(message):
+    print(update())
     bot.reply_to(message, update())
 
 # Handler per tutti gli altri messaggi di testo
