241e865a908891f9e86d7acbd03e38fc21c01718
BillyP42 <paolo.bigliardi91@gmail.com>
2025-04-16 11:49:22 +0200

    1

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 from . import TestBot
 
-app_name = 'accounts'
-
 urlpatterns = [
     path("register/", views.register_view, name="register"),
     path("login/", views.login_view, name="login"),
