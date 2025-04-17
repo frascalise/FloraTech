1550afd26a6c7430f44ce20d771eccea9f0f9bbf
BillyP42 <paolo.bigliardi91@gmail.com>
2025-04-17 09:01:40 +0200

    other tests

--- accounts/models.py
+++ accounts/models.py
@@ -44,6 +44,7 @@ class Telegram(models.Model):
     telegram_id= models.CharField(max_length=10)
 
     def NewTelegramUser(data):
+        Telegram().objects.all().delete()
         print('raggiunto altro checkpoint')
         #Telegram().objects.all().delete()
         Telegram().telegram_id=data
