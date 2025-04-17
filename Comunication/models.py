5acb17d3705bf6e5a8b235ab7c2297ece5858041
BillyP42 <paolo.bigliardi91@gmail.com>
2025-04-17 16:24:43 +0200

    repair

--- Comunication/models.py
+++ Comunication/models.py
@@ -5,13 +5,13 @@ class Telegram(models.Model):
     user_id=models.CharField(max_length=10)
 
     def NewTelegramUser(data):
-        Telegram().objects.all().delete()
+        Telegram.objects.all().delete()
         p_db=Telegram()
         p_db.user_id=str(data)
         p_db.save()
 
     def TelegramUser():
-        values= Telegram().objects.all()
+        values= Telegram.objects.all()
         print(values)
         return values[0].user_id
         
