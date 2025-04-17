6c58456dd4b08dcd7e3509d65a1f8339e856efa5
BillyP42 <paolo.bigliardi91@gmail.com>
2025-04-17 08:49:10 +0200

    another test

--- accounts/models.py
+++ accounts/models.py
@@ -44,7 +44,9 @@ class Telegram(models.Model):
     telegram_id= models.CharField(max_length=10)
 
     def NewTelegramUser(data):
+        Telegram().objects.all().delete()
         Telegram().telegram_id=data
+        print(Telegram().telegram_id)
         Telegram().save()
     
 class Garden(models.Model):
