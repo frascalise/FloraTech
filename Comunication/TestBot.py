1eb9f8d660085566d1177db6ca97a7823e313be0
BillyP42 <paolo.bigliardi91@gmail.com>
2025-04-23 16:56:19 +0200

    correction

--- Comunication/TestBot.py
+++ Comunication/TestBot.py
@@ -46,7 +46,7 @@ def AddNewUser(message):
 def VerifyCode(message):
     bot.reply_to(message,f'Qui ci arriviamo con {message.text}')
     number=random.randint(100000,999999)
-    user_state[message.chat.id]={'control':number}
+    user_state[message.chat.id]={'control':str(number)}
     value=VerifyTelegramUser(message.text,message.chat.id,number)
     bot.send_message(message.chat.id,value)
     
