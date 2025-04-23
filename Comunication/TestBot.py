4bc6eccbdd90528d898a2f512cea8780f3aeba26
BillyP42 <paolo.bigliardi91@gmail.com>
2025-04-23 20:51:20 +0200

    correzione

--- Comunication/TestBot.py
+++ Comunication/TestBot.py
@@ -121,7 +121,7 @@ def Alert(request,problema,username):
         case 'sensor' : bot.send_message(value,'[WARNING]\nComunicazione con sensore assente')
         case 'hub': bot.send_message(value,'[WARNING]\nProblems from the Hub')
         case 'storm': bot.send_message(value,'[WARNING]\n A storm is arriving in few hours')
-        
+    return HttpResponse(status=200)
 
 
 
